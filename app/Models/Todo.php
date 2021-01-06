e89006109ba5a6d8521ce668bd9d666362463260
agumaxwellforcode <agumaxwell9@gmail.com>
2021-01-06 09:19:00 +0100

    Defined model parameters

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'body', 'start', 'due', 'status'
+    ];
 }
--- app/Models/TodoStatus.php
+++ app/Models/TodoStatus.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class TodoStatus extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'slug'
+    ];
 }
